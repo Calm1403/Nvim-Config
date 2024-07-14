e6ecb04eb037221a494704883053290f979ae110
Calm1403 <harris.berry@outlook.com>
2024-07-14 19:22:16 +0100

    Added commentary.

--- init.lua
+++ init.lua
@@ -212,6 +212,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.mypy.with({
+      -- This ensures mypy works within virtual environments.
       extra_args = function()
         local virtual = os.getenv("VIRTUAL_ENV") or "/usr"
         return { "--python-executable", virtual .. "/bin/python3" }
